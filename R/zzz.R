a6438ef9c407fa77105044b21db9d812ef674d0b
Cait <cait.harrigan@mail.utoronto.ca>
2018-09-25 16:05:40 -0400

    template update

--- R/zzz.R
+++ R/zzz.R
@@ -28,7 +28,7 @@
 .onAttach <- function(libname, pkgname) {
   # Startup message
   m <- character()
-  m[1] <- "\nWelcome to <your package name>.\n"
+  m[1] <- "\nWelcome to pointszr.\n"
 
   packageStartupMessage(paste(m, collapse=""))
 }
