52f5f03a4a7fff16624adda74e3e03d4d259355b
Cait <cait.harrigan@mail.utoronto.ca>
2018-10-04 21:44:58 -0400

    plot a volcano from a DDS result object

---
+++ R/vplot.R
@@ -0,0 +1,26 @@
+# vplot.R
+
+#' \code{vplot} create a volcano plot with point size variation
+#'
+#' \code{vplot}
+#'
+#' @param DDSresult a DESeqResults object
+#' @param szModifier a vector containing the variable that point size is varied based on
+#' @param method a string that selects the method to use when scaling point size
+#' @param colSig colour points based on significance. Red if padj<0.05, orange of log2FC>1, green if both. (from http://www.gettinggeneticsdone.com/2014/05/r-volcano-plots-to-visualize-rnaseq-microarray.html)
+#' @param ... arguments to pass to plot()
+#' @examples
+#' plot(c(0.1,10), c(0,1))
+#' abline(v = lseq(0.1, 10, length.out = 20), col="#3366FF66")
+#' @export
+vplot <- function(DDSresult, szModifier, method, colSig=T, ...) {
+  if(colSig == T){
+    with(DDSresult, plot(log2FoldChange, -log10(pvalue), ...))
+    with(subset(DDSresult, padj<.05 ), points(log2FoldChange, -log10(pvalue), pch=20, col="red"))
+    with(subset(DDSresult, abs(log2FoldChange)>1), points(log2FoldChange, -log10(pvalue), pch=20, col="orange"))
+    with(subset(DDSresult, padj<.05 & abs(log2FoldChange)>1), points(log2FoldChange, -log10(pvalue), pch=20, col="green"))
+  }
+  else{
+    with(DDSresult, plot(log2FoldChange, -log10(pvalue), ...))
+  }
+}
