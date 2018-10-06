f28953d6ead569c2f42c34b480570bdb0168ae5a
Cait <cait.harrigan@mail.utoronto.ca>
2018-10-06 16:11:25 -0400

    examples with data for vplot, overlay

--- R/overlay.R
+++ R/overlay.R
@@ -6,12 +6,18 @@
 #'
 #' @param DDSresult a DESeqResults object
 #' @param szMod sizing modifier, a numerical vector to be applied to the elements of DDSResult. Vectors shorter than the axis they're being applied to will be recycled.
+#' @param pch point style to be displayed. by default pch = 20.
 #' @param ... additional arguments to pass to points()
+#' @examples
+#' library(DESeq2)
+#' simDDS <- DESeq(simDDS)
+#' resAB <- results(simDDS, contrast = c("condition", "A","B"))
+#' resAC <- results(simDDS, contrast = c("condition", "A","C"))
 #'
 #' @export
 #'
-overlay <- function(DDSresult, szMod = 1, ...){
+overlay <- function(DDSresult, szMod = 1, pch = 20, ...){
     szMod <- rep_len(szMod, length.out = dim(DDSresult)[1])
     with(DDSresult, points(log2FoldChange, -log10(pvalue),
-                           cex=szMod, ...))
+                           pch = pch, cex=szMod, ...))
 }
--- R/vplot.R
+++ R/vplot.R
@@ -9,10 +9,17 @@
 #' @param cex point size to be displayed. by default cex = 0.5
 #' @param ... additional arguments to pass to plot().
 #' @return returns the DESeqResults object that was used for plotting.
+#' @seealso DESeq2
+#' @examples
+#' library(DESeq2)
+#' simDDS <- DESeq(simDDS)
+#' res <- results(simDDS)
+#'
 #' @export
 
 vplot <- function(DDSresult, pch = 20, cex = 0.5, ...){
 
   with(DDSresult, plot(log2FoldChange, -log10(pvalue),
                        pch = pch, cex = cex, ...))
+  return(DDSresult)
 }
