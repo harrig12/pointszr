a6aa4b46be495f642b5a0701618c0aeb5940194e
Cait <cait.harrigan@mail.utoronto.ca>
2018-10-07 18:26:46 -0400

    remove incomplete tests

--- tests/testthat/test_overlay.R
+++ tests/testthat/test_overlay.R
@@ -5,19 +5,12 @@ context("overlay")
 # ==== BEGIN SETUP AND PREPARE =================================================
 #
 
-load(system.file("extdata/dev/exampleDDSresult.RData", package = "pointszr"))
-
-
 require(DESeq2)
 simDDS <- DESeq(simDDS)
 res <- results(simDDS)
 
 #
 # ==== END SETUP AND PREPARE ===================================================
-
-test_that("plot has been initialized before overlay called", {
-  expect_error(overlay(res), "plot.new has not been called yet", fixed = T)
-})
 
 #remaining tests require initialized plot
 plot(1:5, xlim = c(0,0.5), ylim = c(0,0.5))
@@ -27,10 +20,6 @@ test_that("expect warning when points exceed plot margins", {
                  "Some of the points overlayed exceed plot margins. Consider resizing base plot", fixed = T)
 })
 
-
-test_that("a sample input prouces the expected output",  {
-  #expect_equal(lseq(1,10, length.out = 5), tmp)
-})
 
 
 # ==== BEGIN TEARDOWN AND RESTORE ==============================================
