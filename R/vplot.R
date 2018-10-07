7ac8e187b9dd5a93135340f9b1c133c83d57bf4a
Cait <cait.harrigan@mail.utoronto.ca>
2018-10-07 14:50:41 -0400

    examples and namespace

--- R/vplot.R
+++ R/vplot.R
@@ -2,7 +2,7 @@
 
 #' \code{vplot} create a volcano plot
 #'
-#' \code{vplot}
+#' \code{vplot} plot the differentially expressed genes, as specified in DDSresult
 #'
 #' @param DDSresult a DESeqResults object
 #' @param pch point style to be displayed. by default pch = 20.
@@ -11,15 +11,17 @@
 #' @return returns the DESeqResults object that was used for plotting.
 #' @seealso DESeq2
 #' @examples
-#' library(DESeq2)
+#' require(DESeq2)
 #' simDDS <- DESeq(simDDS)
 #' res <- results(simDDS)
+#' vplot(res)
 #'
 #' @export
 
 vplot <- function(DDSresult, pch = 20, cex = 0.5, ...){
 
-  with(DDSresult, plot(log2FoldChange, -log10(pvalue),
-                       pch = pch, cex = cex, ...))
+  graphics::plot(DDSresult$log2FoldChange, -log10(DDSresult$pvalue),
+                       pch = pch, cex = cex, ...)
+
   return(DDSresult)
 }
