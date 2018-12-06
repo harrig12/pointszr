3f2b1f44e1483699668ca13d611f55033f27cb89
Cait <cait.harrigan@mail.utoronto.ca>
2018-12-05 22:42:53 -0500

    deploy info

--- R/runPointszrApp.R
+++ R/runPointszrApp.R
@@ -3,7 +3,8 @@
 #' \code{runPointszrApp} launch the shiny app distributed with this package framework
 #'
 #' \code{runPointszrApp} launches the shiny app for which the code has been \cr
-#' placed in \code{./inst/shiny-scripts/pointszrApp/}.
+#' placed in \code{./inst/shiny-scripts/pointszrApp/}. You can also find it \cr
+#' online at https://harrig12.shinyapps.io/pointszrApp/
 #' @export
 
 runPointszrApp <- function() {
